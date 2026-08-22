30966d892e5ea72132494e53a95355956bcb98b0
shaikghouse72 <shaikghouse11@gmail.com>
2026-08-22 17:12:48 +0530

    Update deployment message in server.js

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const server = http.createServer((req, res) => {
         <body>
             <h1>Hello from Manual Node.js!</h1>
             <p>GITGITGITGITGIT</p>
-            <p>NO-FURTHER-INTERACTIONS!! This application is >deployed by Jenkins.</p>
-            <p>Second change done!! This application is >deployed by Jenkins.</p>
+            <p>Done new changes!! This application is >deployed by Jenkins.</p>
             <p>Server: 192.168.0.111</p>
             <p>NEW CHANGE - Jenkins deployment test successful!</p>
         </body>
